26ad73aba40fa75f79c3b64d87d38a659f390c6e
Angelo Trabuco <115112109+JoseErnestoOnGithub@users.noreply.github.com>
2023-03-30 15:44:27 -0700

    Upload a bunch of files for our project

---
+++ ForceCrashFixed/ForceCrashFixed/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Dim zero As Integer = 0
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Try
+            Console.WriteLine(1 \ zero)
+        Catch ex As Exception
+            Console.WriteLine("Unhandled exception: " + ex.Message)
+            Console.WriteLine(ex.StackTrace)
+
+
+            Dim ErrMsg = MessageBox.Show(ex.Message & vbCrLf & vbCrLf & ex.StackTrace, "Uncaught exception was thrown", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+            If ErrMsg = DialogResult.OK Then
+                Process.GetCurrentProcess.Kill()
+
+            End If
+
+        End Try
+    End Sub
+End Class
